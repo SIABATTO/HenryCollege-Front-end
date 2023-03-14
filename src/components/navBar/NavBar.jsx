3c3c9f786adfdb953387276e66ecbab423f55608
davemmWeb <davemm.ku@gmail.com>
2023-03-14 09:13:41 -0500

    add NavBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" color="inherit">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          {/* <MenuIcon /> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <img style={{ width : '45px'}} src="https://assets.soyhenry.com/logos/ISOLOGO_HENRY_BLACK.png" alt="logo" />
+            College
+          </Typography>
+          <Button sx={{ mr: 2 }} color="warning">Henry</Button>
+          <Button sx={{ mr: 2 }} color="inherit">Cursos</Button>
+          <Button sx={{ mr: 2 }} color="inherit">FAQs</Button>
+          <Button sx={{ mr: 2 }} color="inherit">Nosotros</Button>
+          <Button sx={{ mr: 2 ,bgcolor:'#ffff00',color:'#212121'}} variant="contained">Crear Cuenta</Button>
+          <Button sx={{ mr: 2 ,bgcolor:'#212121',color:'#fffde7'}} variant="contained">Iniciar sesión</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
