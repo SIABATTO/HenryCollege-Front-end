454982c7a70cc97d839ecfe888ee5575e050352a
Cata Hernandez Mejia <110860662+Cata-hm@users.noreply.github.com>
2023-03-14 11:59:04 -0500

    Avances de Landing

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -1,10 +1,20 @@
+import { Box } from "@mui/system";
 import React from "react";
+import imageGirl from "../../assets/images/LandingGirl.jpg";
+import styles from "./Landing.module.css";
 
 function Landing() {
   return (
-    <div>
-        Estoy en landing
-    </div>
+    <Box className={styles.container}>
+      <Box className={styles.h}>
+        <h1>Invertimos en tu educación</h1>
+        <h2>Comienza a estudiar Desarrollo Full Stack o Data Science</h2>
+        <p>Despega tu carrera en tecnología. Estudia a remoto y de manera intensiva. ¿Y lo mejor? Solo nos pagas cuando consigues un nuevo trabajo.</p>
+      </Box>
+      <Box>
+        <img style={{ width : '100%'}} src={imageGirl} alt="logo" />
+      </Box>
+    </Box>
   );
 }
 
