6f500c48a84d4f403f890046e09da8641943d9f3
Alejo <jubany4@gmail.com>
2023-03-14 23:28:34 -0300

    ignore package-json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Pagination from './components/paged/Paged'
+import Footer from './components/footer/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -27,6 +28,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Footer/>
+        <Pagination/>
+      </div>
     </div>
   )
 }
