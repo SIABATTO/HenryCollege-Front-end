88ec6ac59363ccbf050c311287c7b3d8a420a028
Alejo <jubany4@gmail.com>
2023-03-16 11:30:24 -0300

    actualized

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
+import IconButton from "@mui/material/IconButton";
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import imageLogo from '../../assets/images/ISOLOGO_HENRY_BLACK.png'
