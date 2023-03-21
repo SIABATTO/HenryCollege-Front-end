6382a142ddf159e49cb96f97c67d102093888861
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-03-21 10:27:58 -0500

    se mejoro el estilo del paginado y las cards

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#FFFF01', //amarillo
-      contrastText: 'black', // color de texto secundario
+      main: "#FFFF01", //amarillo
+      contrastText: "black", // color de texto secundario
     },
     secondary: {
-      main: '#fafafa', //blanco
-      contrastText: 'black', // color de texto secundario
+      main: "#fafafa", //blanco
+      contrastText: "black", // color de texto secundario
     },
     tertiary: {
-      main: '#000000', //negro
-      contrastText: 'white', // color de texto secundario
+      main: "#000000", //negro
+      contrastText: "white", // color de texto secundario
     },
     background: {
-      paper: '#fafafa', //negro
+      paper: "#fafafa", //negro
     },
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <ThemeProvider theme={theme}>
-          <App />
+            <App />
         </ThemeProvider>
       </React.StrictMode>
     </BrowserRouter>
-  </Provider>,
-)
+  </Provider>
+);
