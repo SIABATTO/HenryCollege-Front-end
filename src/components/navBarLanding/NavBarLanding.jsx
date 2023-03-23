a0581cd749e8f5c937212bb8d600c283cd0af9b4
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-03-23 08:51:07 -0500

    Update

--- src/components/navBarLanding/NavBarLanding.jsx
+++ src/components/navBarLanding/NavBarLanding.jsx
@@ -1,5 +1,4 @@
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -7,26 +6,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBarLanding = () => {
-  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ bgcolor: "#fafafa" }}>
-        <Toolbar>
+      <AppBar position="fixed" sx={{ bgcolor: "#fafafa"}}>
+        <Toolbar sx={{ml:6}}>
           <img
             style={{ width: "120px" }}
             src="https://assets.soyhenry.com/henry-landing/assets/Henry/logo.png"
             alt="logo"
           />
           <Typography variant="h6" sx={{ marginLeft: "3%" }}>
-            <Link to={"/henrycollege"}>
-              <Button sx={{ mr: 2 ,bgcolor:'#212121',color:'#fffde7'}} color="secondary">
-                Henry College
+            <Link to={"/courses"}>
+              <Button sx={{ mr: 2 ,bgcolor:'#212121',color:'#fffde7'}} color="primary">
+              Henry College
               </Button>
             </Link>
           </Typography>
         </Toolbar>
       </AppBar>
-    </Box>
   );
 };
 
